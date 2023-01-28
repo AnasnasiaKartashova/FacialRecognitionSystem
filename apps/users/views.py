d1fb19aa549525f5dd398ecca1d44cefdb61af46
anasnasiakartashova <kartashovaanastasia17@gmail.com>
2023-01-28 22:16:07 +0300

    my part

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,8 @@
 import os
 from rest_framework import status
 from rest_framework.views import APIView
-from .models import CustomUser, UserEncoding
-from .serializers import UserSerializers, PhotoSerializer
+from .models import CustomUser, UserEncoding, LateComer
+from .serializers import UserSerializers, PhotoSerializer, LateComerSerializer
 from rest_framework.response import Response
 from apps.functions.train_model import train_model_by_img
 
@@ -29,3 +29,17 @@ class EncodingApiView(APIView):
             os.remove(os.path.join('media', f))
         return Response(status=status.HTTP_200_OK)
 
+
+
+class LateComerAPIView(APIView):
+    def get(self, request):
+        serializer = LateComerSerializer(LateComer.objects.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def post(self, request):
+        serializer = LateComerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
